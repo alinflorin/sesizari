55f2ef0d58d7217ca9a173f4bbbcacca38cc32ac
Alin Florin <alin@qoffice.ro>
2024-12-14 12:37:51 +0200

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Box, Stack } from "@mui/material";
+import { Outlet } from "react-router";
+import Header from "./components/Header";
+
 function App() {
-  return <>adasa sad asd asd dasd</>;
+  return (
+    <Stack sx={{ height: "100%" }}>
+      <Header />
+      <Box sx={{flex: 'auto', minHeight: 0, overflow: 'auto'}}>
+        <Outlet />
+      </Box>
+    </Stack>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,24 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import "./index.css";
-import App from "./App.tsx";
+import "./fonts";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import theme from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider theme={theme}>
+  <ThemeProvider theme={theme} noSsr>
     <CssBaseline />
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </ThemeProvider>
 );
