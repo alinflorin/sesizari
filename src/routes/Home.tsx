7039864950c919412b25ee75df0b2541a0ff2856
Alin Florin <alin@qoffice.ro>
2024-12-14 14:28:55 +0200

    sdf

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,18 +1,50 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { LatLngExpression } from "leaflet";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+
+interface MapControlProps {
+  center: LatLngExpression;
+}
+
+export function MapControl({ center }: MapControlProps) {
+  const map = useMap();
+  map.flyTo(center);
+  return null;
+}
 
 export default function Home() {
+  const [center, setCenter] = useState<LatLngExpression>([45.943161, 24.96676]);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      (p) => {
+        if (p) {
+          setCenter([p.coords.latitude, p.coords.longitude]);
+        }
+      },
+      undefined,
+      {
+        enableHighAccuracy: true,
+      }
+    );
+  }, []);
+
   return (
     <MapContainer
       style={{ height: "100%" }}
-      center={[51.505, -0.09]}
-      zoom={13}
+      center={center}
+      zoom={10}
       scrollWheelZoom={true}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={center}>
+        <MapControl center={center} />
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
