e927e509998ab62e8cfacdf142d447f7c13fce99
Alin Florin <alin@qoffice.ro>
2024-12-15 10:28:12 +0200

    -

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,7 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
-import { auth, db } from "../firebase";
-import { doc, getDoc } from "firebase/firestore";
-import { UserProfile } from "../models/user-profile";
+import { auth } from "../firebase";
 import { AppUser } from "../models/app-user";
 
 
@@ -15,12 +13,6 @@ export default function useAuth() {
         return;
       }
       const appUser: AppUser = u;
-      getDoc(doc(db, "userProfiles/" + u.email)).then((d) => {
-        if (d.exists()) {
-          const userProfile: UserProfile = d.data();
-          appUser.isAdmin = userProfile.isAdmin;
-        }
-      });
       setUser(appUser);
     });
     return () => {
