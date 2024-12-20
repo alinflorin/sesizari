186e6de0a8a8eabd02a3ca09ddeb872f01d5767d
Alin Florin <alin@qoffice.ro>
2024-12-20 10:02:14 +0200

    -

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter, Route } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./routes/Home.tsx";
 import NotFound from "./routes/NotFound.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <Route element={<App />}>
-      <Route path="/" index element={<Home />} />
+    <Routes>
+      <Route element={<App />}>
+        <Route path="/" index element={<Home />} />
 
-      <Route path="*" element={<NotFound />} />
-    </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   </BrowserRouter>
 );
