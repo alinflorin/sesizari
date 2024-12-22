85679648c5f8923ecd99f7d7547bc5a7bfea6cd1
Alin Florin <alin@qoffice.ro>
2024-12-22 09:22:31 +0200

    asd

--- src/providers/i18n.ts
+++ src/providers/i18n.ts
@@ -1,28 +1,14 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
 
 export const supportedLanguages = [
   { code: "ro", name: "Română", default: true },
   { code: "en", name: "English", default: false },
 ];
 
-const ld = new LanguageDetector(undefined, {
-  convertDetectedLanguage: l => {
-    if (!l) {
-      return "ro";
-    }
-    if (l.indexOf("-") === -1) {
-      return l;
-    }
-    return l.substring(0, l.indexOf("-"));
-  }
-});
-
 i18n
   .use(Backend)
-  .use(ld)
   .use(initReactI18next)
   .init({
     fallbackLng: "ro",
